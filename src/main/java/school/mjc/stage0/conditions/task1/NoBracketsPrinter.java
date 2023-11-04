0aea303707980d6d076610d0ba10eae1ffe8dbf2
Станислав <stasxp3@gmail.com>
2023-11-04 23:28:54 +0100

    321

--- src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task1;
 public class NoBracketsPrinter {
     public static void main(String[] args) {
         if(false)
-            System.out.println("The statement is true");
-        System.out.println("I'm out of checking");
+        {System.out.println("The statement is true");
+        System.out.println("I'm out of checking");}
     }
 }
